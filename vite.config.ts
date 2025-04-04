57ea37669b75bcbe043b140ef04ec3bf503f3e47
Sven Hemmer <sven.hemmer@hiqs.de>
2025-04-04 23:16:51 +0200

    Fix pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/ld57/',
+  // base: '/ld57/',  // uncomment for local stuff, e.g. npm run preview
+  base: '',           // uncomment for github pages
   publicDir: 'assets',
   build: {
     assetsInlineLimit: 0
