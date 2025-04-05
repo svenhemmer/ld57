fd9d5fcefe6456093e20d090429dbd14cf35e35f
Simon Lenz <post@simon-lenz.de>
2025-04-05 14:57:40 +0200

    cleanup

---
+++ src/GameScene.ts
@@ -0,0 +1,76 @@
+import { Hero } from './hero'
+
+type Layer = {
+    name: string
+    collisionRects: Phaser.GameObjects.Rectangle[]
+    tilemapLayer: Phaser.Tilemaps.TilemapLayer
+    blurEffect?: Phaser.FX.Blur
+}
+
+export class GameScene extends Phaser.Scene {
+    layers: Layer[] = []
+    hero: Hero | null = null
+
+
+    private currentLayer: number = 1
+    private currentLayerCollisions: Phaser.Physics.Arcade.Collider | null = null
+
+    constructor(name: string) {
+        super(name);
+    }
+
+    create() {
+        this.initControls()
+    }
+
+    changeLayer(newLayer: number) {
+        // Remove existing blurs on all layers
+        this.layers.forEach((_layer, i) => {
+            this.layers[i].tilemapLayer.postFX.remove(this.layers[i].blurEffect!)
+        })
+        // Add new blur to all layers
+        this.layers.forEach((_layer, i) => {
+            this.layers[i].blurEffect = this.layers[i].tilemapLayer.postFX.addBlur()
+        })
+        // unblur active layer
+        this.layers[newLayer].tilemapLayer.postFX.remove(this.layers[newLayer].blurEffect!)
+
+        this.currentLayerCollisions?.destroy()
+        this.currentLayerCollisions = this.physics.add.collider(this.hero!, this.layers![newLayer].collisionRects)
+        this.currentLayer = newLayer
+    }
+
+    zoomIn() {
+        const newLayer = Math.max(this.currentLayer - 1 , 0)
+        if (newLayer === this.currentLayer) {
+            return
+        }
+
+        this.changeLayer(newLayer)
+    }
+
+    zoomOut() {
+        const newLayer = Math.min(this.currentLayer + 1 , this.layers.length - 1)
+        if (newLayer === this.currentLayer) {
+            return
+        }
+
+        this.changeLayer(newLayer)
+    }
+
+    initControls() {
+        if (!this.input.keyboard) {
+            return
+        }
+        const plus = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+        const minus = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+
+        plus.on('down', () => {
+            this.zoomIn()
+        })
+        minus.on('down', () => {
+            this.zoomOut()
+        })
+    }
+
+}
--- src/level1.ts
+++ src/level1.ts
@@ -1,25 +1,12 @@
+import { GameScene } from './GameScene';
 import { Hero } from './hero'
 
-type Layers = 'background' | 'middleground' | 'foreground'
-
-export class Level1 extends Phaser.Scene {
-    image! : Phaser.GameObjects.Image;
-
-    private hero: Hero | null = null
-
-    private layers: {
-        name: Layers
-        collisionRects: Phaser.GameObjects.Rectangle[]
-        tilemapLayer: Phaser.Tilemaps.TilemapLayer
-        blurEffect?: Phaser.FX.Blur
-    }[] = []
-
-    private currentLayer: number = 1
-    private currentLayerCollisions: Phaser.Physics.Arcade.Collider | null = null
+export class Level1 extends GameScene {
 
     constructor() {
         super('Level 1');
     }
+    
     preload() : void {
         this.load.image('base_tiles', 'lvl1/terrain_atlas.png')
 
@@ -27,6 +14,7 @@ export class Level1 extends Phaser.Scene {
 
         this.load.image('hero', 'hero.png')
     }
+
     create() : void {
         const lvl1 = this.make.tilemap({ key: 'lvl1' })
 
@@ -47,73 +35,33 @@ export class Level1 extends Phaser.Scene {
                 .map(rect => this.physics.add.existing(rect, true))   
         }
 
+        const bg = lvl1.createLayer('background', tileset, 0, 0)!
         this.layers.push({
-            name:'background',
+            name: 'background',
             collisionRects: addCollisionLayer('background collisions'),
-            tilemapLayer: lvl1.createLayer('background', tileset, 0, 0)!,
+            tilemapLayer: bg,
+            blurEffect: bg.postFX.addBlur()
         })
+        const mg = lvl1.createLayer('middleground', tileset, 0, 0)!
         this.layers.push({
-            name:'middleground',
+            name: 'middleground',
             collisionRects: addCollisionLayer('middleground collisions'),
-            tilemapLayer: lvl1.createLayer('middleground', tileset, 0, 0)!
+            tilemapLayer: mg,
+            blurEffect: mg.postFX.addBlur()
         })
+        const fg = lvl1.createLayer('foreground', tileset, 0, 0)!
         this.layers.push({
-            name:'foreground',
+            name: 'foreground',
             collisionRects: addCollisionLayer('foreground collisions'),
-            tilemapLayer: lvl1.createLayer('foreground', tileset, 0, 0)!
+            tilemapLayer: fg,
+            blurEffect: fg.postFX.addBlur(),
         })
-
-        this.currentLayer = 1
 
         this.hero = new Hero(this, 100, 50)
 
         this.changeLayer(1)
 
-        this.initControls()
-    }
-
-    changeLayer(newLayer: number) {
-        const currentBlurEffect = this.layers[this.currentLayer].blurEffect
-        if (currentBlurEffect) {
-            this.layers![this.currentLayer].tilemapLayer.postFX.remove(currentBlurEffect)
-        }
-        this.currentLayer = newLayer
-        this.currentLayerCollisions?.destroy()
-        this.currentLayerCollisions = this.physics.add.collider(this.hero!, this.layers![this.currentLayer].collisionRects)
-        this.layers[this.currentLayer].blurEffect = this.layers![this.currentLayer].tilemapLayer.postFX.addBlur()
-    }
-
-    zoomIn() {
-        const newLayer = Math.max(this.currentLayer - 1 , 0)
-        if (newLayer === this.currentLayer) {
-            return
-        }
-
-        this.changeLayer(newLayer)
-    }
-
-    zoomOut() {
-        const newLayer = Math.min(this.currentLayer + 1 , this.layers.length - 1)
-        if (newLayer === this.currentLayer) {
-            return
-        }
-
-        this.changeLayer(newLayer)
-    }
-
-    initControls() {
-        if (!this.input.keyboard) {
-            return
-        }
-        const plus = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
-        const minus = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
-
-        plus.on('down', () => {
-            this.zoomIn()
-        })
-        minus.on('down', () => {
-            this.zoomOut()
-        })
+        super.create()
     }
 
     update() : void {
