7ee8d9efc83188901b5546349125da2fc04b79fb
Simon Lenz <post@simon-lenz.de>
2025-04-05 14:25:02 +0200

    Allow zoom in and out (move player with arrow keys, zoom in and out with W and S)

--- src/level1.ts
+++ src/level1.ts
@@ -1,9 +1,21 @@
 import { Hero } from './hero'
 
+type Layers = 'background' | 'middleground' | 'foreground'
+
 export class Level1 extends Phaser.Scene {
     image! : Phaser.GameObjects.Image;
 
     private hero: Hero | null = null
+
+    private layers: {
+        name: Layers
+        collisionRects: Phaser.GameObjects.Rectangle[]
+        tilemapLayer: Phaser.Tilemaps.TilemapLayer
+        blurEffect?: Phaser.FX.Blur
+    }[] = []
+
+    private currentLayer: number = 1
+    private currentLayerCollisions: Phaser.Physics.Arcade.Collider | null = null
 
     constructor() {
         super('Level 1');
@@ -24,14 +36,84 @@ export class Level1 extends Phaser.Scene {
             throw('No tiles found for ' + 'base_tiles');
         }
         
-        const bg = lvl1.createLayer('background', tileset, 0, 0)
-        const mg = lvl1.createLayer('middleground', tileset, 0, 0)
-        const fg = lvl1.createLayer('foreground', tileset, 0, 0)
+        const addCollisionLayer = (key: string) => {
+            return lvl1.getObjectLayer(key)!
+                .objects
+                .map(o => {
+                    const rect = this.add.rectangle(o.x!, o.y!, o.width!, o.height!)
+                    rect.setOrigin(0, 0)
+                    return rect
+                })
+                .map(rect => this.physics.add.existing(rect, true))   
+        }
 
-        bg?.postFX.addBlur()
-        fg?.postFX.addBlur()
+        this.layers.push({
+            name:'background',
+            collisionRects: addCollisionLayer('background collisions'),
+            tilemapLayer: lvl1.createLayer('background', tileset, 0, 0)!,
+        })
+        this.layers.push({
+            name:'middleground',
+            collisionRects: addCollisionLayer('middleground collisions'),
+            tilemapLayer: lvl1.createLayer('middleground', tileset, 0, 0)!
+        })
+        this.layers.push({
+            name:'foreground',
+            collisionRects: addCollisionLayer('foreground collisions'),
+            tilemapLayer: lvl1.createLayer('foreground', tileset, 0, 0)!
+        })
+
+        this.currentLayer = 1
 
         this.hero = new Hero(this, 100, 50)
+
+        this.changeLayer(1)
+
+        this.initControls()
+    }
+
+    changeLayer(newLayer: number) {
+        const currentBlurEffect = this.layers[this.currentLayer].blurEffect
+        if (currentBlurEffect) {
+            this.layers![this.currentLayer].tilemapLayer.postFX.remove(currentBlurEffect)
+        }
+        this.currentLayer = newLayer
+        this.currentLayerCollisions?.destroy()
+        this.currentLayerCollisions = this.physics.add.collider(this.hero!, this.layers![this.currentLayer].collisionRects)
+        this.layers[this.currentLayer].blurEffect = this.layers![this.currentLayer].tilemapLayer.postFX.addBlur()
+    }
+
+    zoomIn() {
+        const newLayer = Math.max(this.currentLayer - 1 , 0)
+        if (newLayer === this.currentLayer) {
+            return
+        }
+
+        this.changeLayer(newLayer)
+    }
+
+    zoomOut() {
+        const newLayer = Math.min(this.currentLayer + 1 , this.layers.length - 1)
+        if (newLayer === this.currentLayer) {
+            return
+        }
+
+        this.changeLayer(newLayer)
+    }
+
+    initControls() {
+        if (!this.input.keyboard) {
+            return
+        }
+        const plus = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+        const minus = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+
+        plus.on('down', () => {
+            this.zoomIn()
+        })
+        minus.on('down', () => {
+            this.zoomOut()
+        })
     }
 
     update() : void {
